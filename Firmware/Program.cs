f4612f30f0acb6e65f57e06518da6a43dd6d58b9
demiurghg <demiurghg@2f246012-c7e7-8381-4342-b932e6a06dd9>
2012-04-13 10:05:06 +0000

    Dev: Firmware

---
+++ Firmware/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Firmware {
+	public class Program {
+		public static void Main ()
+		{
+			// write your code here
+
+
+		}
+
+	}
+}
