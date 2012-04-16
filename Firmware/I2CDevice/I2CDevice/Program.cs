98e8ca13b13249c530dc0552f51d27335147ed4c
alazar.az@gmail.com <alazar.az@gmail.com@2f246012-c7e7-8381-4342-b932e6a06dd9>
2012-04-16 18:46:44 +0000

    Dev: I2CDevice sln added

---
+++ Firmware/I2CDevice/I2CDevice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace I2CDev
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            I2CPlug gyro = new I2CPlug(0x68);
+
+            gyro.WriteToRegister(0x3e, 0x80);
+            Thread.Sleep(100);
+            gyro.WriteToRegister(0x15, 0x00);
+            Thread.Sleep(100);
+            gyro.WriteToRegister(0x16, 0x18);
+            Thread.Sleep(100);
+            //gyro.WriteToRegister(0x17, 0x05);
+
+            byte[] readBuffer = new byte[1] { 0 };
+            int i = 0;
+            for (; ; )
+            {
+                Debug.Print("Iteration : " + i.ToString() + "\n");
+                gyro.ReadFromRegister(0x00, readBuffer);
+                Debug.Print("ID : " + readBuffer[0].ToString() + "\n");
+                gyro.ReadFromRegister(0x1d, readBuffer);
+                Debug.Print(readBuffer[0].ToString() + "\n");
+                gyro.ReadFromRegister(0x1e, readBuffer);
+                Debug.Print(readBuffer[0].ToString() + "\n");
+                Thread.Sleep(100);
+                i++;
+            }
+            
+        }
+
+    }
+}
