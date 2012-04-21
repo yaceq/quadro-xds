974fa23a7006fda804be373a4c532aa3e0409a62
demiurghg <demiurghg@2f246012-c7e7-8381-4342-b932e6a06dd9>
2012-04-21 16:54:37 +0000

    Dev: some stuff

---
+++ trunk/onboardControl/accelerometer/accelerometer.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+ 
+#define MMA7660addr   0x4c
+#define MMA7660_X     0x00
+#define MMA7660_Y     0x01
+#define MMA7660_Z     0x02
+#define MMA7660_TILT  0x03
+#define MMA7660_SRST  0x04
+#define MMA7660_SPCNT 0x05
+#define MMA7660_INTSU 0x06
+#define MMA7660_MODE  0x07
+#define MMA7660_SR    0x08
+#define MMA7660_PDET  0x09
+#define MMA7660_PD    0x0A
+ 
+ 
+class Acceleration
+{
+ public:
+ char x;
+ char y;
+ char z;
+};
+ 
+void sendByte( byte addr, byte value )
+{
+  Wire.beginTransmission( MMA7660addr);
+  Wire.write(addr);   
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+ 
+void mma7660_init(void)
+{
+  Wire.begin();
+  sendByte(MMA7660_MODE, (byte)0x00);
+  sendByte(MMA7660_SR,   (byte)0x00);
+  sendByte(MMA7660_MODE, (byte)0x01);
+}
+ 
+void setup()
+{
+  mma7660_init();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+}
+
+
+int itercount = 0;
+ 
+void Ecom()
+{
+  unsigned char val[3];
+  int count = 0;
+  val[0] = val[1] = val[2] = 64;
+
+  Wire.beginTransmission(MMA7660addr);
+  Wire.write(byte(0x00));  // register to read
+  Wire.endTransmission();
+  Wire.requestFrom(MMA7660addr, 3);    // request 3 bytes from slave device 0x4c
+ 
+  if (Wire.available()) {
+    val[0] = Wire.read();
+    val[1] = Wire.read();
+    val[2] = Wire.read();
+  }
+ 
+  // transform the 7 bit signed number into an 8 bit signed number.
+  Acceleration ret;
+ 
+  ret.x = ((char)(val[0]<<2))/4;
+  ret.y = ((char)(val[1]<<2))/4;
+  ret.z = ((char)(val[2]<<2))/4;
+  Serial.print("accel: ");
+  Serial.print(itercount, DEC);
+  Serial.print(" : ");
+  Serial.print(ret.x, DEC);   // print the reading
+  Serial.print(" ");
+  Serial.print(ret.y, DEC);   // print the reading
+  Serial.print(" ");
+  Serial.print(ret.z, DEC);   // print the reading
+  Serial.println("");
+  itercount++;
+}
+ 
+char reading = 0;
+ 
+void loop()
+{
+  Ecom();
+  delay(20);
+}
---
+++ trunk/onboardControl/escmotor/escmotor.ino
@@ -0,0 +1,90 @@
+#include <Servo.h>
+
+Servo srv1;
+Servo srv2;
+Servo srv3;
+Servo srv4;
+
+void countdown(int time)
+{
+   for (int i=time; i>0; i-=1000) {
+     Serial.print("countdown: ");
+     Serial.println(i/1000);
+     delay(1000);
+   }
+}
+
+
+/*void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Connect the power...");
+  do {
+    delay(100);
+  } while (!Serial.available());
+  byte b = Serial.read();  
+  
+  Serial.println(b, HEX);
+}
+
+
+void loop()
+{
+    Serial.println("Ololo!");
+    delay(1000);
+}
+//*/
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("setup()");
+  srv1.attach(5);   srv1.write(10);
+  srv2.attach(6);   srv2.write(10);
+  srv3.attach(10);  srv3.write(10);
+  srv4.attach(11);  srv4.write(10);
+  Serial.println("Connect the power...");
+  do {
+    delay(100);
+  } while (!Serial.available());
+  byte b;
+  b = Serial.read();  
+  Serial.println(b, HEX);
+  b = Serial.read();  
+  Serial.println(b, HEX);
+}
+
+
+void loop()
+{
+    for (int i=10; i<150; i+=15) {
+      Serial.println(i);
+      srv1.write(i);
+      delay(100);
+    }
+    for (int i=60; i<150; i+=15) {
+      Serial.println(i);
+      srv2.write(i);
+      delay(100);
+    }
+    for (int i=10; i<150; i+=15) {
+      Serial.println(i);
+      srv3.write(i);
+      delay(100);
+    }
+    for (int i=10; i<150; i+=15) {
+      Serial.println(i);
+      srv4.write(i);
+      delay(100);
+    }
+    
+    if (Serial.available()) {
+      srv1.write(10);
+      srv2.write(10);
+      srv3.write(10);
+      srv4.write(10);
+      while(1) {}
+    }
+}
+//*/
